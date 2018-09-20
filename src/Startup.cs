b05aa5b82b5c40c313ca026374ae3fe03d552cbc
Noah <huiali@hotmail.com>
2018-09-20 18:46:21 +0800

    ConfigureRoutes : Set the main route as the connection key.

--- src/Startup.cs
+++ src/Startup.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Huiali.ILOData.Extensions;
+using Huiali.ILOData.Models;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,22 +31,48 @@ namespace Huiali.ILOData
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddOData();
+
+            var connectionSetion = Configuration.GetSection("ConnectionStrings");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder(app.ApplicationServices);
+
+            var connections = this.Configuration.GetSection("ConnectionStrings").GetChildren();
+            Action<IRouteBuilder> configureRoutes = routeBuilder =>
+            {
+                foreach (var item in connections)
+                {
+                    var connectionString = item.Value;
+                    var tables = DbSchemaReader.GetSchemata(connectionString);
+
+                    //Multipl entitySet from tables
+                    //builder.EntitySet<>()
+
+                    routeBuilder.MapODataServiceRoute(
+                        $"ODATAROUTE_{item.Key}",
+                        item.Key,
+                        builder.GetEdmModel());
+                }
+                routeBuilder
+                    .Count()
+                    .Filter()
+                    .OrderBy()
+                    .Expand()
+                    .Select()
+                    .MaxTop(null);
+                routeBuilder.EnableDependencyInjection();
+            };
+
+            app.UseMvc(configureRoutes);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseMvc();
         }
     }
 }
